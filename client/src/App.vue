60038d5ead7e029c9989d789032b5bc5f75db4f1
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-12-19 09:27:25 -0300

    Add Basic Vue Client

---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#/">FullStack Message Board</a>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+</style>
