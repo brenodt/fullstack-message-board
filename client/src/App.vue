2ae8bd162c290d787ed62d083a2d50aca1ab94bf
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-12-20 10:05:11 -0300

    Integrate front with backend

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#/">FullStack Message Board</a>
     </nav>
-    <router-view/>
+    <router-view class="container" />
   </div>
 </template>
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,90 @@
 <template>
+  <!-- eslint-disable max-len -->
   <div>
-    <h1>Full Stack Message Board</h1>
+    <button @click="toggleMessageForm = !toggleMessageForm" type="button" class="btn btn-secondary disabled mb-3 mt-3">Toggle Message Form</button>
+    <form v-if="toggleMessageForm" @submit.prevent="addMessage" class="mb-3">
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input v-model="message.username" type="text" class="form-control" id="username" required>
+      </div>
+      <div class="form-group">
+        <label for="subject">Subject</label>
+        <input v-model="message.subject" type="text" class="form-control" id="subject" placeholder="Enter a subject..." required>
+      </div>
+      <div class="form-group">
+        <label for="message">Message</label>
+        <textarea v-model="message.message" class="form-control" id="message" rows="3" required></textarea>
+      </div>
+      <div class="form-group">
+        <label for="imageURL">Image URL</label>
+        <input v-model="message.imageURL" type="url" class="form-control" id="imageURL" placeholder="Enter an image url..." required >
+      </div>
+      <button type="submit" class="btn btn-primary">Add Message</button>
+    </form>
+    <div class="list-unstyled" v-for="message in reversedMessages" :key="message._id">
+      <div class="media">
+        <img v-if="message.imageURL" :src="message.imageURL" class="mr-3" :alt="message.subject">
+        <div class="media-body">
+          <h4 class="mt-0 mb-1">{{ message.username }}</h4>
+          <h5 class="mt-0 mb-1">{{ message.subject }}</h5>
+          {{ message.message }}
+          <br/>
+          <small>{{ message.created }}</small>
+        </div>
+      </div>
+      <hr>
+    </div>
   </div>
 </template>
 
 <script>
+const API_URL = 'http://localhost:2727/messages';
 
 export default {
   name: 'Home',
+  data: () => ({
+    toggleMessageForm: false,
+    messages: [],
+    message: {
+      username: 'Anonymous',
+      subject: '',
+      message: '',
+      imageURL: '',
+    },
+  }),
+  mounted() {
+    fetch(API_URL).then((response) => response.json()).then((result) => {
+      this.messages = result;
+    });
+  },
+  computed: {
+    reversedMessages() {
+      return this.messages.slice().reverse();
+    },
+  },
+  methods: {
+    addMessage() {
+      fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(this.message),
+        headers: {
+          'content-type': 'application/json',
+        },
+      }).then((response) => response.json()).then((result) => {
+        this.messages.push(result);
+      });
+    },
+  },
 };
 </script>
+
+<style>
+hr {
+  border-top: 1px solid white;
+}
+
+img {
+  max-width: 300px;
+  height: auto;
+}
+</style>
